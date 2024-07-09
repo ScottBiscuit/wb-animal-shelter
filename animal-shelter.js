eb4482467f6bb19749b7c5cf5749bf46f1ace7d9
Scott Johnstone <scottajohnstone@gmail.com>
2024-07-09 15:40:24 -0500

    removed repetition from greetings and food for Cat/Dog/Animal

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,46 +1,50 @@
 const animalData = require('./animal-data.json');
 
 class Animal {
-    constructor(name, species, color, hunger = 50) {
+    constructor(name, species, color, hunger = 50, greeting = 'Hi', food = 'food') {
         this.name = name;
         this.species = species;
         this.color = color;
         this.hunger = hunger;
+        this.greeting = greeting;
+        this.food = food;
     }
     greet(){
-        console.log(`Hi, ${this.name} the ${this.species}`);
+        console.log(`${this.greeting}, I'm ${this.name} the ${this.species}`);
     }
     feed(){
         this.hunger -= 20;
-        console.log(`Yum, I love food!`);
+        console.log(`Yum, I love ${this.food}!`);
     }
 }
 
 class Cat extends Animal {
     constructor(name, color, hunger = 50) {
-        super(name, 'cat', color, hunger);
-        this.food = 'fish';
+        super(name, 'cat', color, hunger, 'Meow', 'fish');
+        // this.greeting = 'Meow';
+        // this.food = 'fish';
     }
-    greet(){
-        console.log(`Meooowww, I'm ${this.name} the adorable ${this.species}!`)
-    }
-    feed(){
-        this.hunger -= 20;
-        console.log(`Yum, I love ${this.food}!`);
-    }
+    // greet(){
+    //     console.log(`Meooowww, I'm ${this.name} the adorable ${this.species}!`)
+    // }
+    // feed(){
+    //     this.hunger -= 20;
+    //     console.log(`Yum, I love ${this.food}!`);
+    // }
 }
 class Dog extends Animal {
     constructor(name, color, hunger = 50) {
-        super(name, 'dog', color, hunger);
-        this.food = 'kibble';
+        super(name, 'dog', color, hunger, 'Woof', 'kibble');
+        // this.greeting = 'Woof';
+        // this.food = 'kibble';
     }
-    greet(){
-        console.log(`Woof, I'm ${this.name} the ${this.species}!`)
-    }
-    feed(){
-        this.hunger -= 20;
-        console.log(`Yum, I love ${this.food}!`);
-    }
+    // greet(){
+    //     console.log(`Woof, I'm ${this.name} the ${this.species}!`)
+    // }
+    // feed(){
+    //     this.hunger -= 20;
+    //     console.log(`Yum, I love ${this.food}!`);
+    // }
 }
 
 class AnimalShelter {
@@ -65,14 +69,19 @@ const shelter = new AnimalShelter();
 for (const a of animalData){
     let animal;
     const hunger = a.hunger ? a.hunger : 50;
+    const greeting = a.greeting ? a.greeting : 'Hi';
+    const food = a.food ? a.food : 'food'
     if (a.species === 'dog'){
-        animal = new Dog(a.name, a.color, hunger);
+        animal = new Dog(a.name, a.color, hunger, greeting, food);
     } else if (a.species === 'cat'){
-        animal = new Cat(a.name, a.color, hunger);
+        animal = new Cat(a.name, a.color, hunger, greeting, food);
     } else {
-        animal = new Animal(a.name, a.species, a.color, hunger);
+        animal = new Animal(a.name, a.species, a.color, hunger, greeting, food);
     }
     shelter.addAnimal(animal)
 }
 
-console.log(shelter.getAnimalsBySpecies('cat'))
+for (const animal of shelter.animals) {
+    animal.greet();
+    animal.feed();
+}
